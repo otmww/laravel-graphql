45e1fb6af7c589e141a656345783caf1ad73a4ca
Qwerty <carelloqwerty@gmail.com>
2022-01-18 11:29:18 +0200

    change-package-requirements: fix missin laravel helper function for laravel^6.0

--- src/Folklore/GraphQL/Support/helpers.php
+++ src/Folklore/GraphQL/Support/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 if (! function_exists('array_get')) {
     /**
@@ -14,5 +15,18 @@ if (! function_exists('array_get')) {
     function array_get($array, $key, $default = null)
     {
         return Arr::get($array, $key, $default);
+    }
+}
+
+if (! function_exists('studly_case')) {
+    /**
+     * Convert a value to studly caps case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    function studly_case($value)
+    {
+        return Str::studly($value);
     }
 }
